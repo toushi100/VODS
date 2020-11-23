e584f1d1c7cf39d6d56ded38048e4cc6ec32bd86
toushi100 <toushi2009@gmail.com>
2020-11-23 21:48:52 +0200

    changes

--- Homeapp/views.py
+++ Homeapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render,HttpResponse
 def home(request):
     return render(request,'Homeapp/home.html')
 
+def about(request):
+    return render(request,'Homeapp/about.html')
+
 # Create your views here.
