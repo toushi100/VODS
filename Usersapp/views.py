7a80fc0577802efc889d496945bfe2a725b3bd08
toushi100 <toushi2009@gmail.com>
2020-11-26 16:24:02 +0200

    added register.html and register function

--- Usersapp/views.py
+++ Usersapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render,HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+def regesiter(request):
+    form = UserCreationForm()
+    return render (request, 'Usersapp/regesiter.html', {'form':form})
 
 def dashboard(request):
     return HttpResponse("dashboard")
