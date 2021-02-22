ce160135d7424c93b5f26277c68e4d19fc127f69
toushi100 <toushi2009@gmail.com>
2021-02-22 02:50:15 +0200

    showing only mp4s when uploading a video

--- Videoapp/forms.py
+++ Videoapp/forms.py
@@ -4,6 +4,7 @@ from .models import Video
 class VideoCreationForm(forms.ModelForm):
     video = forms.FileField()
     name = forms.CharField()
+    video.widget.attrs.update({'accept': 'video/mp4','class':'btn btn-info'})
     class Meta:
         model = Video
         fields = ['name', 'description','video']
