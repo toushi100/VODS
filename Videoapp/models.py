df1f84f60027a5a36204d359a0220672f3d6cae6
toushi100 <toushi2009@gmail.com>
2021-02-25 02:46:49 +0200

    adding the result of detection script into DB

--- Videoapp/models.py
+++ Videoapp/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from  django.contrib.auth.models import User
-import time, subprocess, pickle
+import time, subprocess, pickle, json
 from embed_video.fields import EmbedVideoField 
 from django.db.models.signals import post_save
 
 
+
+
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        path = '/home/ahmed/Desktop/VODS/filename.pickle'
+        video = instance.video
+        video = '/home/ahmed/Desktop/VODS/media/'+str(video)
+        print("detecting is starting")
+        output = subprocess.run(['/home/ahmed/Desktop/SSD/tagroba.py',video])
+        with open(path, 'rb') as handle:
+            b = pickle.load(handle)
+        print(b)
+        b = json.dumps(b)
+        print("adadadsasdasdadsasdadadadsasd",b)
+        VideoObject.objects.create(video = instance ,obj = b)
+    if not created:
+        instance.Videobject.save()
 
 class Video(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
@@ -18,44 +35,27 @@ class Video(models.Model):
     
     def save(self, *args, **kwargs):
         super(Video, self).save(*args, **kwargs)
-        video = self.video
-        video = '/home/ahmed/Desktop/VODS/media/'+str(video)
-        print("detecting is starting")
-        output = subprocess.run(['/home/ahmed/Desktop/SSD/tagroba.py',video])
-        if output.returncode == 0:
-            path = '/home/ahmed/Desktop/SSD/objects.pickle'
-            with open(path, 'rb') as handle:
-                b = pickle.load(handle)
-                print(b)
+        
+post_save.connect(create_profile,sender = Video)
+print('save is done')
+
+                
     
 class VideoObject(models.Model):
     video = models.ForeignKey(Video,on_delete=models.CASCADE)
-    obj = models.TextField()
+    obj = models.JSONField(null = True)
 
     def __str__(self):
         return f'{self.video}'
 
-    '''def save(self, *args, **kwargs):
-        video = self.video.video
-        video = '/home/ahmed/Desktop/VODS/media/'+str(video)
-        output = subprocess.run(['/home/ahmed/Desktop/SSD/tagroba.py',video])
-        if output.returncode == 0:
-            path = '/home/ahmed/Desktop/SSD/objects.pickle'
-            with open(path, 'rb') as handle:
-                b = pickle.load(handle)'''
+
+            
+            
+        
 
 
 
-def create_profile(sender,instance,created,**kwargs):
-    if created:
-        VideoObject.objects.create(video=instance)
-        print("object created")
-
-post_save.connect(create_profile,sender = Video)
-
-
-def save_profile(sender, instance,created, **kwargs):
-    if not created:
-        instance.Videobject.save()
-
-post_save.connect(save_profile,sender = Video)
+# def save_profile(sender, instance,created, **kwargs):
+#      if not created:
+#         instance.Videobject.save()
+# post_save.connect(save_profile,sender = Video)
