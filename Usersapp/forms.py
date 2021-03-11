c80f18bc6cd8df91e430468e3aa30985bf92fa9e
toushi100 <toushi2009@gmail.com>
2021-03-11 07:50:00 +0200

    fixed form in register.html and added first name and last name in form.py

--- Usersapp/forms.py
+++ Usersapp/forms.py
@@ -2,19 +2,45 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Row, Layout, Column, Submit, Fieldset
+
+
 class UserResgisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
+
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper(self)
+    #     self.helper.layout = Layout(
+    #         Row(
+    #             Column('first_name', css_class='form-group col-md-6 mb-0'),
+    #             Column('last_name', css_class='form-group col-md-6 mb-0'),
+    #             css_class='form-row'
+    #         ),
+    #         'username',
+    #         'email',
+    #         Row(
+    #             Column('password1', css_class='form-group col-md-6 mb-0'),
+    #             Column('password2', css_class='form-group col-md-6 mb-0'),
+    #             css_class='form-row'
+    #         )
+    #     )
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['username', 'email']
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
